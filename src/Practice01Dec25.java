d712d8375ad838ed3d1a988d77ca5e5a837d0480
Mohd Danish <mddanish.cse@gmail.com>
2023-12-25 12:04:55 +0530

    Initial commit

--- src/Practice01Dec25.java
+++ src/Practice01Dec25.java
@@ -1,13 +1,24 @@
+import java.util.ArrayList;
+
 public class Practice01Dec25 {
     public static void main(String[] args){
         int a=5;
         Integer b =10;
-        int d=b.compareTo(b);
+        System.out.println(a+b);
         String test = "testing";
         int c =a+b;
         int[] ar={1,2};
         int[][] ar2 = new int[2][2];
         ar2[1][1]=0;
-        System.out.println(ar2);
+        System.out.println(ar2+" "+ar2[1][1]);
+
+//        Array List
+        ArrayList<Integer> arl1 = new ArrayList<Integer>();
+        System.out.println(arl1);
+        arl1.add(2);
+        arl1.add(2);
+        arl1.add(2,25);
+        System.out.println(arl1);
+        System.out.println(arl1.get(2));
     }
 }
