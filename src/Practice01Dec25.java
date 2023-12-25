7d327f97c5fb70b91bfbf4fa553cffbb7ab145ee
Mohd Danish <mddanish.cse@gmail.com>
2023-12-25 11:57:26 +0530

    Initial commit

---
+++ src/Practice01Dec25.java
@@ -0,0 +1,13 @@
+public class Practice01Dec25 {
+    public static void main(String[] args){
+        int a=5;
+        Integer b =10;
+        int d=b.compareTo(b);
+        String test = "testing";
+        int c =a+b;
+        int[] ar={1,2};
+        int[][] ar2 = new int[2][2];
+        ar2[1][1]=0;
+        System.out.println(ar2);
+    }
+}
